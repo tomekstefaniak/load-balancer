8eb051d1de0c730a37d9f5ab66fd4a419131572c
Tomasz Stefaniak <tomaszstefaniak2718@gmail.com>
2025-09-13 23:20:00 +0200

    starting, stopping, destructor

--- include/core/LoadBalancer.hpp
+++ include/core/LoadBalancer.hpp
@@ -2,20 +2,29 @@
 
 #include <mutex>
 #include <shared_mutex>
+#include <thread>
+#include <atomic>
+#include <memory>
+#include <boost/asio/ip/tcp.hpp>
 #include "../config/Config.hpp"
 #include "../strategy/IStrategy.hpp"
+#include "../core/SessionsWorker.hpp"
 
-enum State {
+// There are exactly as many sessions workers as logic threads on the machine
+const unsigned int NO_OF_CORES = std::thread::hardware_concurrency();
+
+enum State
+{
     IDLE,
-    BUSY
+    ACTIVE
 };
 
 class LoadBalancer
 {
 public:
     // Forbid copying LoadBalancer instance
-    LoadBalancer(const LoadBalancer&) = delete;
-    LoadBalancer& operator=(const LoadBalancer&) = delete;
+    LoadBalancer(const LoadBalancer &) = delete;
+    LoadBalancer &operator=(const LoadBalancer &) = delete;
 
     /**
      * @brief Set instance of LoadBalancer with provided config
@@ -23,10 +32,10 @@ public:
      * @throws std::runtime_exception When instance already exists
      * @return Pointer to LoadBalancer instance
      */
-    static LoadBalancer* SetInstance(Config config);
+    static LoadBalancer *SetInstance(Config config);
 
     // Get instance
-    static LoadBalancer* GetInstance();
+    static LoadBalancer *GetInstance();
 
     /**
      * @breief Create and add a new server to LoadBalancer
@@ -40,7 +49,16 @@ public:
     // Return list of servers configs objects
     std::vector<ServerConfig> GetServers();
 
-    // Start balancing load
+    /**
+     * @brief Signal server on connection close for strategy update
+     * @param server Server to signal
+     */
+    void Signal(std::shared_ptr<ServerConfig> server);
+
+    /**
+     * @brief Start balancing load When balancer has already started
+     * @throws std::runtime_exception
+     */
     void StartWork();
 
     // Stop balancing load
@@ -53,20 +71,41 @@ public:
 
 private:
     // Mutex for synchronization of singleton creation
-    static std::mutex* singletonMutex_;
-    // Signleton instance
-    static LoadBalancer* instance;
+    static std::mutex *singletonMutex_;
+    // Singleton instance
+    static LoadBalancer *instance;
+
+    // Port
+    uint16_t clientsPort_;
 
     // Mutex for managing state
-    std::mutex* stateMutex_;
+    std::mutex stateMutex_;
     // State
     State state_;
 
     // Mutex for operations on servers in strategy, name comes from servers_ field that requires syncing
-    std::shared_mutex* serversMutex_;
+    std::shared_mutex serversMutex_;
 
     // Strategy of balancing load
-    IStrategy* strategy_;
+    IStrategy *strategy_;
+
+    // List of sessions workers instances
+    std::vector<SessionsWorker *> workers_;
+
+    // List of sessions workers thread
+    std::vector<std::thread *> workersThreads_;
+
+    // Round robin index for sessions workers
+    std::atomic<uint32_t> currentWorkerIndex_{0};
+
+    // Context for accepting new clients connections
+    std::shared_ptr<boost::asio::io_context> balancerContext_;
+
+    // Acceptor for new tcp connections
+    std::unique_ptr<boost::asio::ip::tcp::acceptor> acceptor_;
+
+    // Thread for running balancer context
+    std::thread *balancerThread_;
 
     /**
      * @brief Sets fields from config, creates sockets one for each server and one clients
@@ -74,4 +113,15 @@ private:
      * @param config
      */
     LoadBalancer(Config config);
+
+    /**
+     * @brief Get next sessions worker using round robin
+     * @return Pointer to the next SessionsWorker
+     */
+    SessionsWorker *getNextWorker();
+
+    /**
+     * @brief Start accepting new client connections
+     */
+    void startAccepting();
 };
--- src/core/LoadBalancer.cpp
+++ src/core/LoadBalancer.cpp
@@ -1,5 +1,8 @@
 #include <stdexcept>
 #include "core/LoadBalancer.hpp"
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/ip/address.hpp>
+#include <boost/asio/io_context.hpp>
 #include "strategy/LeastConnections.hpp"
 #include "strategy/RoundRobin.hpp"
 
@@ -7,19 +10,28 @@ LoadBalancer *LoadBalancer::instance = nullptr;
 std::mutex *LoadBalancer::singletonMutex_ = new std::mutex;
 
 LoadBalancer::LoadBalancer(Config config)
-    : strategy_(), stateMutex_(new std::mutex), state_(IDLE), serversMutex_(new std::shared_mutex())
+    : strategy_(), state_(IDLE), balancerContext_(std::make_shared<boost::asio::io_context>()),
+      acceptor_(nullptr), balancerThread_(nullptr)
 {
     // Create strategy
-    if (config.algorithmName == "leastconnections") {
+    if (config.algorithmName == "leastconnections")
+    {
         strategy_ = new LeastConnections(config.serversConfigs);
-    } else if (config.algorithmName == "roundrobin") {
-        strategy_ = new RoundRobin(config.serversConfigs);
-    } else {
-        throw std::invalid_argument("uknown strategy");
     }
+    else if (config.algorithmName == "roundrobin")
+    {
+        strategy_ = new RoundRobin(config.serversConfigs);
+    }
+    else
+    {
+        throw std::invalid_argument("unknown strategy");
+    }
+
+    // Set clients port
+    clientsPort_ = config.clientsPort;
 }
 
-LoadBalancer* LoadBalancer::SetInstance(Config config)
+LoadBalancer *LoadBalancer::SetInstance(Config config)
 {
     // Quick check in case instance already exists
     if (instance == nullptr)
@@ -44,35 +56,263 @@ LoadBalancer *LoadBalancer::GetInstance()
 void LoadBalancer::AttachServer(ServerConfig serverConfig)
 {
     // Lock & enter safely
-    std::unique_lock<std::shared_mutex> lock(*serversMutex_);
+    std::unique_lock<std::shared_mutex> lock(serversMutex_);
     strategy_->AttachServer(serverConfig);
 }
 
 void LoadBalancer::DettachServer(ServerConfig serverConfig)
 {
     // Lock & enter safely
-    std::unique_lock<std::shared_mutex> lock(*serversMutex_);
+    std::unique_lock<std::shared_mutex> lock(serversMutex_);
     strategy_->DettachServer(serverConfig);
 }
 
 std::vector<ServerConfig> LoadBalancer::GetServers()
 {
     // Lock & enter safely
-    std::shared_lock<std::shared_mutex> lock(*serversMutex_);
+    std::shared_lock<std::shared_mutex> lock(serversMutex_);
     return strategy_->GetServers();
+}
+
+void LoadBalancer::Signal(std::shared_ptr<ServerConfig> server)
+{
+    // Lock & enter safely
+    std::unique_lock<std::shared_mutex> lock(serversMutex_);
+    strategy_->Signal(server);
 }
 
 void LoadBalancer::StartWork()
 {
+    {
+        // Lock state mutex because we set state to ACTIVE
+        std::lock_guard<std::mutex> lock(stateMutex_);
 
+        if (state_ != IDLE)
+        {
+            throw std::runtime_error("LoadBalancer instance has already started");
+        }
+
+        // Start sessions workers
+        for (int i = 0; i < NO_OF_CORES; i++)
+        {
+            auto worker = new SessionsWorker(this);
+
+            auto workerThread = new std::thread(
+                [worker]()
+                { worker->StartWork(); });
+
+            // Push back new session worker
+            workers_.push_back(worker);
+            // Push back new thread
+            workersThreads_.push_back(workerThread);
+        }
+
+        state_ = ACTIVE;
+    }
+
+    // Create acceptor for new tcp connections
+    acceptor_ = std::make_unique<boost::asio::ip::tcp::acceptor>(
+        *balancerContext_,
+        boost::asio::ip::tcp::endpoint(boost::asio::ip::tcp::v4(), clientsPort_));
+
+    // Start accepting connections
+    startAccepting();
+
+    balancerThread_ = new std::thread(
+        [this]()
+        { balancerContext_->run(); } // Run the io_context
+    );
+}
+
+SessionsWorker *LoadBalancer::getNextWorker()
+{
+    if (workers_.empty())
+    {
+        return nullptr;
+    }
+
+    size_t index = currentWorkerIndex_.fetch_add(1) % workers_.size();
+    return workers_[index];
+}
+
+void LoadBalancer::startAccepting()
+{
+    // Get next SessionsWorker using round robin
+    if (SessionsWorker *worker = getNextWorker())
+    {
+        // Create new socket for client connection
+        auto clientSocket = std::make_shared<boost::asio::ip::tcp::socket>(*(worker->workerContext));
+
+        acceptor_->async_accept(
+            *clientSocket,
+            [this, worker, clientSocket](const boost::system::error_code &ec)
+            {
+                if (!ec)
+                {
+                    // Get next server using strategy
+                    std::shared_lock<std::shared_mutex> lock(serversMutex_);
+                    auto server = strategy_->Next();
+                    lock.unlock();
+
+                    if (server)
+                    {
+                        // Create server socket and connect to server
+                        auto serverSocket = std::make_shared<boost::asio::ip::tcp::socket>(*(worker->workerContext));
+
+                        boost::asio::ip::tcp::endpoint serverEndpoint(
+                            boost::asio::ip::make_address(server->serverIP),
+                            server->serverPort);
+
+                        serverSocket->async_connect(
+                            serverEndpoint,
+                            [this, worker, clientSocket, serverSocket, server](const boost::system::error_code &connectEc)
+                            {
+                                if (!connectEc)
+                                {
+                                    // Pass both sockets and server config to SessionsWorker
+                                    worker->NewSession(std::move(*clientSocket), std::move(*serverSocket), server);
+                                }
+                                else
+                                {
+                                    // Connection to server failed, close client socket
+                                    clientSocket->close();
+                                }
+                            });
+                    }
+                    else
+                    {
+                        // No available server, close client connection
+                        clientSocket->close();
+                    }
+                }
+
+                // Continue accepting new connections
+                startAccepting();
+            });
+    }
+    else
+    {
+        throw std::runtime_error("no sessions workers available");
+    }
 }
 
 void LoadBalancer::StopWork()
 {
+    // Lock state mutex because we change state
+    std::lock_guard<std::mutex> lock(stateMutex_);
 
+    if (state_ != ACTIVE)
+    {
+        throw std::runtime_error("LoadBalancer is not active");
+    }
+
+    // Stop balancer context
+    if (balancerContext_)
+    {
+        balancerContext_->stop();
+    }
+
+    // Join balancer thread
+    if (balancerThread_ && balancerThread_->joinable())
+    {
+        balancerThread_->join();
+        delete balancerThread_;
+    }
+
+    // Stop all worker contexts
+    for (auto *worker : workers_)
+    {
+        if (worker && worker->workerContext)
+        {
+            worker->workerContext->stop();
+        }
+    }
+
+    // Join all worker threads
+    for (auto *thread : workersThreads_)
+    {
+        if (thread && thread->joinable())
+        {
+            thread->join();
+            delete thread;
+        }
+    }
+
+    // Clean up workers
+    for (auto *worker : workers_)
+    {
+        if (worker)
+        {
+            delete worker;
+        }
+    }
+
+    // Clear containers
+    workers_.clear();
+    workersThreads_.clear();
+
+    // Reset contexts
+    balancerContext_.reset();
+    balancerContext_ = std::make_shared<boost::asio::io_context>();
+
+    // Reset strategy state
+    {
+        std::unique_lock<std::shared_mutex> lock(serversMutex_);
+        if (strategy_)
+        {
+            strategy_->Reset();
+        }
+    }
+
+    state_ = IDLE;
 }
 
 LoadBalancer::~LoadBalancer()
 {
+    // Stop balancer if it's still active
+    if (state_ == ACTIVE)
+    {
+        try
+        {
+            StopWork();
+        }
+        catch (...)
+        {
+            // Ignore exceptions in destructor - just ensure cleanup
+        }
+    }
 
+    // Delete strategy and its resources
+    if (strategy_)
+    {
+        delete strategy_;
+        strategy_ = nullptr;
+    }
+
+    // Clean up any remaining workers (should be empty after StopWork)
+    for (auto *worker : workers_)
+    {
+        if (worker)
+        {
+            delete worker;
+        }
+    }
+    workers_.clear();
+
+    // Clean up any remaining threads (should be empty after StopWork)
+    for (auto *thread : workersThreads_)
+    {
+        if (thread)
+        {
+            if (thread->joinable())
+            {
+                thread->join();
+            }
+            delete thread;
+        }
+    }
+    workersThreads_.clear();
+
+    // Reset singleton instance
+    instance = nullptr;
 }
