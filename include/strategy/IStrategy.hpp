cead7ce4fcce59a5e1a009b600ab6c65fa08806c
Tomasz Stefaniak <tomaszstefaniak2718@gmail.com>
2025-09-13 23:25:11 +0200

    default destructor

--- include/strategy/IStrategy.hpp
+++ include/strategy/IStrategy.hpp
@@ -1,13 +1,13 @@
 #pragma once
 
 #include <memory>
-#include "strategy/AttachedServer.hpp"
+#include "config/Config.hpp"
 
 class IStrategy
 {
 public:
     // Get next attached server
-    virtual std::shared_ptr<AttachedServer> Next() = 0;
+    virtual std::shared_ptr<ServerConfig> Next() = 0;
 
     // Add new server to the group, if already in strategy do nothing
     virtual void AttachServer(ServerConfig serverConfig) = 0;
@@ -19,10 +19,13 @@ public:
     virtual std::vector<ServerConfig> GetServers() = 0;
 
     // Signal the server on connection close
-    virtual void Signal(std::shared_ptr<AttachedServer> server) = 0;
+    virtual void Signal(std::shared_ptr<ServerConfig> server) = 0;
+
+    // Reset strategy state (counters, indices, etc.)
+    virtual void Reset() = 0;
 
     virtual ~IStrategy() = 0;
 };
 
-// Even pure virtual destructors need an implementation
+// Implementation of pure virtual destructor
 inline IStrategy::~IStrategy() = default;
